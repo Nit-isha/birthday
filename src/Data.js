63319dd090688aa54fc6f0269eed2f7c277f9afb
Nitisha <nitishagupta.1999@gmail.com>
2021-12-04 18:49:08 +0530

    feat: project

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+import nitisha from "./images/nitisha.jpg";
+import celestina from "./images/celestina.jpeg";
+import tripti from "./images/tripti.jpeg";
+import swikriti from "./images/swikriti.jpeg";
+
+export default [
+  {
+    id: 1,
+    name: "Nitisha Gupta",
+    age: 22,
+    image: nitisha,
+  },
+  {
+    id: 2,
+    name: "Celestina Meghan",
+    age: 21,
+    image: celestina,
+  },
+  {
+    id: 3,
+    name: "Tripti Yadav",
+    age: 22,
+    image: tripti,
+  },
+  {
+    id: 4,
+    name: "Swikriti Tiwari",
+    age: 22,
+    image: swikriti,
+  },
+];
